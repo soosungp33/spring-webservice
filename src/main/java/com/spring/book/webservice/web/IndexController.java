21b0e14d6823982bd5746ab205c8f934809f3842
soosungp33 <soosungp33@gmail.com>
2021-01-19 22:55:13 +0900

    chapter 4 - 머스테치로 화면 구성하기

---
+++ src/main/java/com/spring/book/webservice/web/IndexController.java
@@ -0,0 +1,13 @@
+package com.spring.book.webservice.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController { // 머스테치 스타터 덕분에 앞의 경로와 뒤의 파일 확장자는 자동으로 지정된다.
+
+    @GetMapping("/")
+    public String index() {
+        return "index"; // src/main/resources/templates/index.mustache 로 전환되어 처리
+    }
+}
