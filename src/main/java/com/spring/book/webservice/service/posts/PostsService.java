cf1d191ea210fc52f51edb52506620f1f2f19a47
soosungp33 <soosungp33@gmail.com>
2021-01-20 18:13:51 +0900

    chapter 4 - 전체 조회화면 만들기

--- src/main/java/com/spring/book/webservice/service/posts/PostsService.java
+++ src/main/java/com/spring/book/webservice/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.spring.book.webservice.service.posts;
 
 import com.spring.book.webservice.domain.posts.Posts;
 import com.spring.book.webservice.domain.posts.PostsRepository;
+import com.spring.book.webservice.web.dto.PostsListResponseDto;
 import com.spring.book.webservice.web.dto.PostsResponseDto;
 import com.spring.book.webservice.web.dto.PostsSaveRequestDto;
 import com.spring.book.webservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +38,12 @@ public class PostsService {
                 IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/spring/book/webservice/web/IndexController.java
+++ src/main/java/com/spring/book/webservice/web/IndexController.java
@@ -1,13 +1,20 @@
 package com.spring.book.webservice.web;
 
+import com.spring.book.webservice.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController { // 머스테치 스타터 덕분에 앞의 경로와 뒤의 파일 확장자는 자동으로 지정된다.
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) { // Model에 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장
+        model.addAttribute("posts", postsService.findAllDesc()); // findAllDesc로 가져온 결과를 posts로 index.mustache에 전달
         return "index"; // src/main/resources/templates/index.mustache 로 전환되어 처리
     }
 
