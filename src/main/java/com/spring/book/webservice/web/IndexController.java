3ecd98b9ad62878022bdae16abb32ffc193de0f4
soosungp33 <soosungp33@gmail.com>
2021-01-21 13:31:01 +0900

    chapter 4 - 게시글 수정, 삭제 화면 만들기

--- src/main/java/com/spring/book/webservice/web/IndexController.java
+++ src/main/java/com/spring/book/webservice/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.spring.book.webservice.web;
 
 import com.spring.book.webservice.service.posts.PostsService;
+import com.spring.book.webservice.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,5 +23,13 @@ public class IndexController { // 머스테치 스타터 덕분에 앞의 경로
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
